72d623c8caaab8c3317b87e32721d9ac69c0330f
Bilge Adam <bilge.adam@baogrenci.com>
2020-08-19 15:58:11 +0300

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.w3schools.com/cs/cs_data_types.asp
+            //https://www.tutorialsteacher.com/csharp/csharp-data-types
+            //https://www.tutorialsteacher.com/csharp/csharp-value-type-and-reference-type
+
+            string ad = "Çağatay";
+
+            int x = -22;
+            short s = 141;
+            long l = 66;
+
+            float q = -1.456F;
+            double y = 22.1;
+            decimal z = 2.5M;
+
+            bool durum = true;
+            char karakter = 'c';
+
+            string surname;
+
+            surname = "Yıldız";
+            surname = "Alaca";
+
+        }
+    }
+}
+
